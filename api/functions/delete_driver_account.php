54a63ee40d86dfb0dab6e5ebb34fa29bf0caa33c
Daniel <danibarrantesv33@gmail.com>
2026-04-19 21:24:01 -0600

    Run PHP via Vercel api and rewrite includes endpoints

---
+++ api/functions/delete_driver_account.php
@@ -0,0 +1,2 @@
+<?php
+require_once __DIR__ . '/../../includes/functions/delete_driver_account.php';
---
+++ api/functions/update_driver_account.php
@@ -0,0 +1,2 @@
+<?php
+require_once __DIR__ . '/../../includes/functions/update_driver_account.php';
